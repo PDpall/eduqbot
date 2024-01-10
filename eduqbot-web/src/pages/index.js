5478e8893bccf63c2686eef965f71a4693292149
PDpall <135216217+PDpall@users.noreply.github.com>
2024-01-10 11:53:52 +0100

    Update index.js

--- eduqbot-web/src/pages/index.js
+++ eduqbot-web/src/pages/index.js
@@ -1,30 +1,40 @@
 
-
-
 import { useState } from "react";
+
+
+const SYSTEM_MESSAGE = "You are an AI assistant eduqbot";
+
+
 
 export default function Home() {
   const [apiKey, setApiKey] = useState("");
 
-
+  const [botMessage, setBotMessage] = useState("");
  
 
-  const API_URL = "https://api.openai.com/v1/chat/completions";
-  
-  const [botMessage, setBotMessage] = useState("");
+  const API_URL = "https://api.openai.com/v1/chat/completions" ;
+
+
 
   const sendRequest = async () => {
     const response = await fetch(API_URL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${apiKey}`,
+        Authorization: `Bearer ` + apiKey,
       },
       body: JSON.stringify({
         model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: "Hello!" }],
+        messages: [
+          { role: "system", content: "SYSTEM MESSAGE"},
+          { role: "user", content: "Hello, Could you please Introduce yourself"}],
       }),
     });
+
+    const responseJson = await response.json();
+    setBotMessage(responseJson.choices[0].message.content);  
+    console.log("botMessage", botMessage);
+
   }
 
   return (
@@ -44,18 +54,31 @@ export default function Home() {
           </div>
         </div>
       </nav>
+      {/* History Message */}
+
+      <div className="flex-1">
+        <div className= "max-w-screen-md mx-auto w-full text-indigo-800">Message History</div>
+        </div>
+
+        {/* Input box */}
+        <div>
+        <div className="max-w-screen-md mx-auto w-full flex">
+          <textarea className="border text-lg rounded-md flex-1" /> 
+</div>
+        </div>
+
+      
+
 
      <div className="p-4">
       <button className="w-40 bordered rounded bg-blue-500 hover:bg-indigo-700 text-white p-2"
       onClick={sendRequest}>Send Request</button>
+      <div className="mt-4 text-lg">{botMessage}
+      </div>
 
      </div>
     </div>
+
+ 
   );
 }
-
-     <div className="p-4">
-      <button className="border rounded-md p-2">Send Request</button>
-
-     </div>
-    </div>
