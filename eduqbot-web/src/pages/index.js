6f4493dc7be90a990d9193df6589f49defba98ca
PDpall <135216217+PDpall@users.noreply.github.com>
2024-01-12 14:45:02 +0100

    Update index.js

--- eduqbot-web/src/pages/index.js
+++ eduqbot-web/src/pages/index.js
@@ -1,12 +1,32 @@
 import { useState } from "react"; 
 
+const SYSTEM_MESSAGE = "You are Eduqbot, an AI Assistant created using state of the art ML models and APIs";
+
 export default function Home() {
 
   const [apiKey, setApiKey] = useState("");
+  const [botMessage, setBotMessage] = useState("");
+  const [userMessage, setUserMessage] = useState("");
+
+  const [messages, setMessages] = useState([
+    {
+      role: "system",
+      content:
+        "You are Eduqbot, a helpful AI developed and powered by state-of-the-art machine learning models.",
+    },
+  ]);
   const API_URL = "https://api.openai.com/v1/chat/completions";
   /*console.log ("apiKey", apiKey);*/
   
   async function sendRequest () { 
+    const newMessage = { role: "user", content: "userMessage"};
+    const newMessages = [
+      ...messages,
+      newMessage
+    ]
+    setMessages(newMessages);
+    setUserMessage("");
+  
     const response = await fetch (API_URL, {
       method:"POST",
       headers:{
@@ -15,12 +35,14 @@ export default function Home() {
       },
       body: JSON.stringify ({
         model:"gpt-3.5-turbo",
-        messages:  [{role: "user", content: "Hello!" }],
+        messages:newMessages,
       }),
     });
 
     const responseJson = await response.json ();
-    console.log ('responseJson', responseJson);
+    setBotMessage(responseJson.choices[0].message.content);
+    
+
   } 
 
 
@@ -36,14 +58,50 @@ return <div className="flex flex-col h-screen">
         />
       </div>
     </nav>
-    <div className ="px-4 py-4"> 
-      <button className="border rounded-md p-2 bg-blue-500  hover:bg-blue-600 text-white"
-      onClick={sendRequest}> 
-        Send Request 
-      </button>
-    </div>
-  </div>
+
+    <div className="flex-1 overflow-y-scroll">
+        <div className= "max-w-screen-md mx-auto w-full text-indigo-800">
+
+          {messages.map((message ,idx) => (
+            <div key={idx} className="my-3">
+
+              <div className="font-bold">{message.role}</div>
+              <div className="font-l">{message.content}</div>
+            </div>)
+          )}
+        </div>
+        </div>
+       
+
+
+        {/* Input box */}
+        <div>
+        
+        <div className="max-w-screen-md mx-auto w-full flex px-4 pb-4">
+          <textarea
+          value={userMessage}
+          onChange={(e) => setUserMessage(e.target.value)}
+          className="border text-lg rounded-md flex-1" rows={1} />
+           <button onClick={sendRequest} 
+           className="w-40 bordered rounded bg-blue-500 hover:bg-indigo-700 text-white p-2">Send</button>
+                   
+</div>
+        </div>
+
+      
+
+     
+     </div>
+ 
+
+ 
+
 }
+
+
+
+
+ 
 
 
 
